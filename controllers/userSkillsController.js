615aa0c53c3c42099f9ad553ac329e53c328f966
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-04-29 16:45:18 -0700

    remove innecessary userskills index route and control

--- controllers/userSkillsController.js
+++ controllers/userSkillsController.js
@@ -1,16 +1,5 @@
 const knex = require("knex")(require("../knexfile"));
 
-//get a user's skills	
-exports.index = (_req, res) => {
-	knex("userskills")
-		.then((data) => {
-			res.status(200).json(data);
-		})
-		.catch((err) =>
-			res.status(400).send(`Error retrieving users: ${err}`)
-		);
-};
-
 //add new user's skills to userskills table
 exports.newUser = async (req, res) => {
 	try {
