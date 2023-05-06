a6ac6cfaa36a0768e2049b8c3588027f173928f0
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-05-06 10:57:47 -0700

    start on factoring out hardcoded queries on UsersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,16 +2,28 @@ const knex = require("knex")(require("../knexfile"));
 
 //return user who logged in and all neighbors and skills for those neighbors
 exports.index = async (req, res) => {
+	const whereClause = { email: req.body.email };
+	const joinClause = {
+		table: "userskills",
+		joinCondition: function () {
+			this.on("users.user_id", "=", "userskills.user_id");
+		},
+	};
 	try {
 		//find user who logged in
-		const foundUser = await knex("users").where({ email: req.body.email });
-		if (foundUser.length === 0) {res.status(404).send(`No user found with email ${req.body.email}`)}
-		else {
+		const foundUser = await knex("users").where(whereClause);
+		if (foundUser.length === 0) {
+			return res.status(404).send(`No user found with email ${req.body.email}`);
+		} else {
 			//if found user, find all neighbors within 1/2 km as neighbors
 			const neighbors = await knex("users")
 				//join userskills table and users table on user_id
-				.join("userskills", "users.user_id", "=", "userskills.user_id")
+				// .join("userskills", "users.user_id", "=", "userskills.user_id")
+				.join(joinClause.table, joinClause.joinCondition)
 				//select all columns from users table and select all skills and offers from userskills table labeled as barters
+
+			//============NEED TO UPDATE THIS QUERY BELOW THIS LINE TO TAKE VARIABLES AS OPPOSED TO HARD CODED VALUES================
+
 				.select(
 					"users.user_id",
 					"users.about",
@@ -25,7 +37,7 @@ exports.index = async (req, res) => {
 					"users.city",
 					"users.province",
 					"users.address",
-					"users.created_at",
+					"users.created_at"
 				)
 				.select(
 					knex.raw(
@@ -43,84 +55,84 @@ exports.index = async (req, res) => {
 			res.status(200).json(neighbors);
 		}
 	} catch (err) {
-		res.status(404).send(`Error getting user ${err}`);
+		return res.status(404).send(`Error getting user ${err}`);
 	}
 };
 
 //create a new user
 exports.newUser = async (req, res) => {
+
+	const newUserData = {
+		user_id: req.body.user_id,
+		about: req.body.about,
+		email: req.body.email,
+		first_name: req.body.first_name,
+		last_name: req.body.last_name,
+		location: knex.raw("POINT(?, ?)", [req.body.coords[0], req.body.coords[1]]),
+		password: req.body.password,
+		image_url: req.body.image_url,
+		status: req.body.status,
+		home: req.body.home,
+		city: req.body.city,
+		province: req.body.province,
+		address: req.body.address,
+	};
+
 	try {
-		await knex("users").insert({
-			user_id: req.body.user_id,
-			about: req.body.about,
-			email: req.body.email,
-			first_name: req.body.first_name,
-			last_name: req.body.last_name,
-			location: knex.raw("POINT(?, ?)", [
-				req.body.coords[0],
-				req.body.coords[1],
-			]),
-			password: req.body.password,
-			image_url: req.body.image_url,
-			status: req.body.status,
-			home: req.body.home,
-			city: req.body.city,
-			province: req.body.province,
-			address: req.body.address,
-		});
-		const newUser = await knex("users")
-			.where("user_id", req.body.user_id)
-			.first();
+		await knex("users").insert(newUserData);
+
+		const whereClause = { user_id: req.body.user_id };
+		const newUser = await knex("users").where(whereClause).first();
+
 		res.json(newUser);
 	} catch (err) {
 		console.error(err);
-		res.status(400).send(`Error adding new user ${err}`);
+		return res.status(400).send(`Error adding new user ${err}`);
 	}
 };
 
 //edit a user's information
 exports.editUser = async (req, res) => {
+
+	const whereClause = { user_id: req.body.user_id };
+	const updateData = {
+		user_id: req.body.user_id,
+		about: req.body.about,
+		email: req.body.email,
+		first_name: req.body.first_name,
+		last_name: req.body.last_name,
+		location: knex.raw("POINT(?, ?)", [req.body.coords[0], req.body.coords[1]]),
+		password: req.body.password,
+		image_url: req.body.image_url,
+		status: req.body.status,
+		home: req.body.home,
+		city: req.body.city,
+		province: req.body.province,
+		address: req.body.address,
+	};
+
 	try {
-		await knex("users")
-			.where("user_id", req.body.user_id)
-			.first()
-			.update({
-				user_id: req.body.user_id,
-				about: req.body.about,
-				email: req.body.email,
-				first_name: req.body.first_name,
-				last_name: req.body.last_name,
-				location: knex.raw("POINT(?, ?)", [
-					req.body.coords[0],
-					req.body.coords[1],
-				]),
-				password: req.body.password,
-				image_url: req.body.image_url,
-				status: req.body.status,
-				home: req.body.home,
-				city: req.body.city,
-				province: req.body.province,
-				address: req.body.address,
-			});
+		await knex("users").where(whereClause).first().update(updateData);
 		const editedUser = await knex("users")
 			.where("user_id", req.body.user_id)
 			.first();
 		res.json(editedUser);
 	} catch (err) {
 		console.error(err);
-		res.status(400).send(`Error editing user ${err}`);
+		return res.status(400).send(`Error editing user ${err}`);
 	}
 };
 
 exports.addImage = async (req, res) => {
-	// const imageData = req.body.imageData; // Extract the image data from the request body
+	//match user_id to user_id in database
+	const whereClause = { user_id: req.body.user_id };
+	//create object with image_url for update
+	const updateData = { image_url: req.file.filename };
 	try {
-		await knex("users")
-			.where("user_id", req.body.user_id)
-			.update({ image_url: req.file.filename }); // Insert the image data into the database using Knex
+		await knex("users").where(whereClause).update(updateData); // Insert the image data into the database
 		res.status(200).json({ message: "Image uploaded successfully" }); // Send a success response back to the client
 	} catch (err) {
 		console.error(err);
-		res.status(500).json({ error: err.message }); // Send an error response back to the client
+		return res.status(500).json({ error: err.message }); // Send an error response back to the client if unsuccessful
 	}
 };
