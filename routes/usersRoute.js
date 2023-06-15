84bb4b0aef73790b60d8025949af5d5008c4d3aa
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-06-15 07:01:36 -0700

    update formatting of usersRoute.js

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -2,6 +2,31 @@ const router = require('express').Router();
 const usersController = require('../controllers/usersController.js');
 const multer  = require('multer');
 
+router
+.route('/')
+//get all neighbbors route based off of email in auth header and location
+.get(usersController.getNeighbors)
+//edit user route
+.put(usersController.editUser)
+//new user post request route
+.post(usersController.newUser);
+
+//route to login user
+router
+.route('/login')
+.post(usersController.login);
+
+//route to verify user
+router
+.route('/verify')
+.get(usersController.verifyUser);
+
+//route to check if email exists in database
+router
+.route('/newemail')
+.post(usersController.newEmail);
+
+//all multer file upload route settings and route
 //multer maximum upload size configuration
 const maxSize = 1000000;
 
@@ -20,31 +45,6 @@ let storage = multer.diskStorage({
     storage: storage,
     limits: { fileSize: maxSize }
   });
-
-  //post request to get all nearby users
-router
-.route('/')
-//get all neighbbors route based off of email in auth header and location
-.get(usersController.getNeighbors)
-//edit user route
-.put(usersController.editUser)
-//new user post request route
-.post(usersController.newUser);
-
-//change to .post/login instead of .post/users/login
-router
-.route('/login')
-.post(usersController.login);
-
-router
-.route('/verify')
-.get(usersController.verifyUser);
-
-//newEmail to check if email exists in database
-//can I do this as a function in auth.js instead of it being a route?
-router
-.route('/newemail')
-.post(usersController.newEmail);
 
 //add image to user route
 router
