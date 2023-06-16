5b464fd24fe8730933e05d781f155b89397c5eb8
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-06-15 20:21:43 -0700

    modify userSkillsController to receive all offers and barters as one skillsArray, map through each skill and insert it into the userSkills table with one call

--- controllers/userSkillsController.js
+++ controllers/userSkillsController.js
@@ -1,16 +1,33 @@
 const knex = require("knex")(require("../knexfile"));
 
 //add new user's skills to userskills table
+// exports.newUser = async (req, res) => {
+// 	try {
+// 		await knex("userskills").insert({
+// 			user_id: req.body.user_id,
+// 			skill: req.body.skill,
+// 			offer: req.body.offer,
+// 		});
+// 		res.status(200).send("User skills added");
+// 	} catch (err) {
+// 		res.status(400).send(`Error adding new user skills ${err}`);
+// 	}
+// };
+
+//map each skill and desire out and insert them into the userskills table
 exports.newUser = async (req, res) => {
+	const skillsArray = req.body.skills;
+	const userSkills = skillsArray.map((skill) => ({
+		user_id: req.body.user_id,
+		skill: skill.skill,
+		offer: skill.offer,
+	}));
+
 	try {
-		await knex("userskills").insert({
-			user_id: req.body.user_id,
-			skill: req.body.skill,
-			offer: req.body.offer,
-		});
+		await knex("userskills").insert(userSkills);
 		res.status(200).send("User skills added");
 	} catch (err) {
-		res.status(400).send(`Error adding new user skills ${err}`);
+		res.status(400).send(`Error adding new user skills: ${err}`);
 	}
 };
 
@@ -22,4 +39,4 @@ exports.removeSkills = async (req, res) => {
 	} catch (err) {
 		res.status(400).send(`Error removing user skills ${err}`);
 	}
-}
+};
