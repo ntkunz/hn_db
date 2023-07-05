5b9cfe40a74162b4dcde5c956f21233a30f60694
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-07-05 05:01:27 -0700

    add DB_HOST variable to .env copy and to knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,54 +1,53 @@
-require('dotenv').config();
+require("dotenv").config();
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-  client: 'mysql2',
-  connection: {
-    host: '127.0.0.1',
-    database: process.env.DB_LOCAL_DBNAME,
-    user: process.env.DB_LOCAL_USER,
-    password: process.env.DB_LOCAL_PASSWORD,
-  },
+	client: "mysql2",
+	connection: {
+		host: process.env.DB_HOST,
+		database: process.env.DB_LOCAL_DBNAME,
+		user: process.env.DB_LOCAL_USER,
+		password: process.env.DB_LOCAL_PASSWORD,
+	},
 };
 
-  // development: {
-  //   client: 'sqlite3',
-  //   connection: {
-  //     filename: './dev.sqlite3'
-  //   }
-  // },
+// development: {
+//   client: 'sqlite3',
+//   connection: {
+//     filename: './dev.sqlite3'
+//   }
+// },
 
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
+// staging: {
+//   client: 'postgresql',
+//   connection: {
+//     database: 'my_db',
+//     user:     'username',
+//     password: 'password'
+//   },
+//   pool: {
+//     min: 2,
+//     max: 10
+//   },
+//   migrations: {
+//     tableName: 'knex_migrations'
+//   }
+// },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
-
+// production: {
+//   client: 'postgresql',
+//   connection: {
+//     database: 'my_db',
+//     user:     'username',
+//     password: 'password'
+//   },
+//   pool: {
+//     min: 2,
+//     max: 10
+//   },
+//   migrations: {
+//     tableName: 'knex_migrations'
+//   }
+// }
