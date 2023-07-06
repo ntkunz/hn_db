ed725c25d506c24264c9be7ca56b07ab2ab4265a
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-07-06 15:55:08 -0700

    set knexfile for helping-neighbors-test heroku deploy

--- knexfile.js
+++ knexfile.js
@@ -6,48 +6,19 @@ require("dotenv").config();
 module.exports = {
 	client: "mysql2",
 	connection: {
-		host: process.env.DB_HOST,
+		host: "127.0.0.1",
 		database: process.env.DB_LOCAL_DBNAME,
 		user: process.env.DB_LOCAL_USER,
 		password: process.env.DB_LOCAL_PASSWORD,
 	},
+	production: {
+		client: "mysql2",
+		connection: process.env.DATABASE_URL || process.env.CLEARDB_DATABASE_URL,
+		migrations: {
+			directory: __dirname + "/migrations",
+		},
+		seeds: {
+			directory: __dirname + "/seeds",
+		},
+	},
 };
-
-// development: {
-//   client: 'sqlite3',
-//   connection: {
-//     filename: './dev.sqlite3'
-//   }
-// },
-
-// staging: {
-//   client: 'postgresql',
-//   connection: {
-//     database: 'my_db',
-//     user:     'username',
-//     password: 'password'
-//   },
-//   pool: {
-//     min: 2,
-//     max: 10
-//   },
-//   migrations: {
-//     tableName: 'knex_migrations'
-//   }
-// },
-
-// production: {
-//   client: 'postgresql',
-//   connection: {
-//     database: 'my_db',
-//     user:     'username',
-//     password: 'password'
-//   },
-//   pool: {
-//     min: 2,
-//     max: 10
-//   },
-//   migrations: {
-//     tableName: 'knex_migrations'
-//   }
-// }
