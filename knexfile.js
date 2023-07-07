8ad5c37818ae9456e822f17d9bcfbea967851427
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-07-07 05:56:35 -0700

    remove host from knexfile connection

--- knexfile.js
+++ knexfile.js
@@ -16,7 +16,7 @@ module.exports = {
 		client: "mysql2",
 		// connection: process.env.DATABASE_URL,
     connection: {
-      host : 'process.env.DATABASE_URL',
+      // host : 'process.env.DATABASE_URL',
       port : 'process.env.DB_PORT',
       user : 'process.env.DB_USER',
       password : 'process.env.DB_PASSWORD',
