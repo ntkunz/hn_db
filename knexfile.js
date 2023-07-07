413ba831be2b96d9d6f7b56c542514bae7311aa8
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-07-07 11:51:49 -0700

    add ssl true to connection knexfile

--- knexfile.js
+++ knexfile.js
@@ -14,7 +14,7 @@ module.exports = {
   client: "mysql2",
 	production: {
 		client: "mysql2",
-		connection: process.env.DATABASE_URL,
+		connection: process.env.DATABASE_URL + '?ssl=true',
     pool: {
       min: 2,
       max: 10
