895afe736916ee31553ca2dfbb221bfe19852ac8
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-07-07 06:16:23 -0700

    change connection in knexfile

--- knexfile.js
+++ knexfile.js
@@ -14,14 +14,14 @@ module.exports = {
   client: "mysql2",
 	production: {
 		client: "mysql2",
-		// connection: process.env.DATABASE_URL,
-    connection: {
-      host : 'process.env.DATABASE_URL',
-      port : 'process.env.DB_PORT',
-      user : 'process.env.DB_USER',
-      password : 'process.env.DB_PASSWORD',
-      database : 'process.env.DB_DBNAME',
-    },
+		connection: process.env.DATABASE_URL,
+    // connection: {
+    //   host : 'process.env.DATABASE_URL',
+    //   port : 'process.env.DB_PORT',
+    //   user : 'process.env.DB_USER',
+    //   password : 'process.env.DB_PASSWORD',
+    //   database : 'process.env.DB_DBNAME',
+    // },
 		migrations: {
 			directory: __dirname + "/migrations",
 		},
