617a9bb259130e7119655474129b7f8178014138
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-07-08 15:57:26 -0700

    adjust which mysql2 is active in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -15,9 +15,9 @@ module.exports = {
 	// };
 
 	// below used for deployment
-	// client: "mysql2",
+	client: "mysql2",
 	production: {
-		client: "mysql2",
+		// client: "mysql2",
 		connection: process.env.CLEARDB_DATABASE_URL + "?ssl=true",
 		pool: {
 			min: 2,
