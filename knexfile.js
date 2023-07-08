f7ecbd488e6a633982c83dacfe90cb295a4cef7d
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-07-08 15:56:09 -0700

    remove mysql2 from knexfile

--- knexfile.js
+++ knexfile.js
@@ -15,7 +15,7 @@ module.exports = {
 	// };
 
 	// below used for deployment
-	client: "mysql2",
+	// client: "mysql2",
 	production: {
 		client: "mysql2",
 		connection: process.env.CLEARDB_DATABASE_URL + "?ssl=true",
