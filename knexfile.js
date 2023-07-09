5bfaacae6848f582fe1e2b7c687a993b66bd205b
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-07-08 18:17:24 -0700

    add ssl rejection and debug to production knexfile

--- knexfile.js
+++ knexfile.js
@@ -18,16 +18,25 @@ module.exports = {
 	client: "mysql2",
 	production: {
 		client: "mysql2",
+
+		connection: {
+			connectionString: process.env.DATABASE_URL,
+			ssl: {
+				rejectUnauthorized: false,
+			},
+			debug: true,
+		},
+
 		// connection: process.env.CLEARDB_DATABASE_URL + "?ssl=true",
-		connection: process.env.DATABASE_URL,
+		// connection: process.env.DATABASE_URL,
 		// connection: {
-//   host : 'process.env.JAWSDB_URL',
-//   host : 'process.env.DATABASE_URL',
-//   port : 'process.env.DB_PORT',
-//   user : 'process.env.DB_USER',
-//   password : 'process.env.DB_PASSWORD',
-//   database : 'process.env.DB_DBNAME',
-// },
+		//   host : 'process.env.JAWSDB_URL',
+		//   host : 'process.env.DATABASE_URL',
+		//   port : 'process.env.DB_PORT',
+		//   user : 'process.env.DB_USER',
+		//   password : 'process.env.DB_PASSWORD',
+		//   database : 'process.env.DB_DBNAME',
+		// },
 		pool: {
 			min: 2,
 			max: 10,
