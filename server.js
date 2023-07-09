69dbe035f3fc66383d5ab879650118c9efdc6db3
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-07-09 13:28:41 -0700

    remove commented out and unused code from server.js. Initial commit to cleanut from feature-heroku-prep

--- server.js
+++ server.js
@@ -1,50 +1,25 @@
-const knex = require("knex")(require("./knexfile"));
 const cors = require("cors");
 const helmet = require("helmet");
-const bcrypt = require("bcryptjs");
 const rateLimit = require("express-rate-limit");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const { protect } = require("./modules/auth");
 
-const mysql = require('mysql2');
-
-//SSL SETTING EXAMPLE
-// const connection = mysql.createConnection({
-//   host: 'your-hostname',
-//   user: 'your-username',
-//   password: 'your-password',
-//   database: 'your-database',
-//   ssl: {
-//     ca: 'path/to/ca.pem',
-//     cert: 'path/to/client-cert.pem',
-//     key: 'path/to/client-key.pem',
-//   },
-// });
-
 const corsOptions = {
-	// origin: "https://loquacious-vacherin-531c19.netlify.app/*",
 	origin: "https://main--loquacious-vacherin-531c19.netlify.app",
-	// origin: "*",
 };
 
 const limiter = rateLimit({
-	// windowMs: 15 * 60 * 1000, // 15 minutes
 	windowMs: 60 * 1000, // 1 minute
 	max: 60, // Limit each IP to 60 requests per `window` (here, per 1 minute)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
 
-// app.use(cors({
-// 	origin: process.env.CORS_ORIGIN
-// }));
-
 app.use(function (req, res, next) {
-	// res.header("Access-Control-Allow-Origin", "https://loquacious-vacherin-531c19.netlify.app/*"); // update to match the domain you will make the request from
-	res.header("Access-Control-Allow-Origin", "https://main--loquacious-vacherin-531c19.netlify.app"); // update to match the domain you will make the request from
-	res.header(
+	res.header("Access-Control-Allow-Origin", "https://main--loquacious-vacherin-531c19.netlify.app"); 
+		res.header(
 		"Access-Control-Allow-Headers",
 		"Origin, X-Requested-With, Content-Type, Accept"
 	);
@@ -53,9 +28,9 @@ app.use(function (req, res, next) {
 
 app.use(cors(corsOptions));
 
-// app.use(cors());
 app.use(express.json());
 app.use(express.static('public/images'));
+//below left here until certain routing will work without it
 // app.use(express.static(__dirname + "./../build"));
 app.use(helmet());
 app.use(limiter);
@@ -73,6 +48,7 @@ app.use("/messages", protect, messageRoutes);
 app.use("/users", protect, userRoutes);
 app.use("/userskills", protect, userSkillsRoutes);
 
+//catch-all to make sure all routes work since frontend is single page application
 app.get("*", (_req, res) => {
 	res.sendFile("index.html", { root: __dirname + "./../build" });
 });
