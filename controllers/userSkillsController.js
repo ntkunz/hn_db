978cbed43e5152b8e12466a6617ad359dac3530e
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-07-29 11:52:20 -0700

    update var names coming from client

--- controllers/userSkillsController.js
+++ controllers/userSkillsController.js
@@ -11,7 +11,7 @@ exports.newUser = async (req, res) => {
 
 	// Map each skill, desire, and user_id
 	const userSkills = skillsArray.map((skill) => ({
-		user_id: req.body.user_id,
+		user_id: req.body.userId,
 		skill: skill.skill,
 		offer: skill.offer,
 	}));
@@ -20,13 +20,12 @@ exports.newUser = async (req, res) => {
 		await knex("userskills").insert(userSkills);
 		return res.status(200).send("User skills added");
 	} catch (err) {
-		return res.status(400).send(`Error adding new user skills: ${err}`);
+		console.log("error adding new user skills: ", err);
+		return res.status(400).send(`Error adding new user skills`);
 	}
 };
 
 /**
- * Remove a user's skills from the userskills table
- *
  * @param {Object} req - The HTTP request object
  * @param {Object} res - The HTTP response object
  * @returns {Object} - The HTTP response with a status code and message
@@ -36,6 +35,7 @@ exports.removeSkills = async (req, res) => {
 		await knex("userskills").where("user_id", req.params.id).del();
 		return res.status(200).send("User skills removed");
 	} catch (err) {
-		return res.status(400).send(`Error removing user skills ${err}`);
+		console.log("Error removing user skills: ", err);
+		return res.status(400).send(`Error removing user skills`);
 	}
 };
--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const { protect } = require("./modules/auth");
 const corsOptions = {
-	// origin: "https://main--loquacious-vacherin-531c19.netlify.app",
 	origin: process.env.CLIENT_URL,
 };
 
@@ -19,7 +18,6 @@ const limiter = rateLimit({
 });
 
 app.use(function (req, res, next) {
-	// res.header("Access-Control-Allow-Origin", "https://main--loquacious-vacherin-531c19.netlify.app"); 
 	res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL); 
 		res.header(
 		"Access-Control-Allow-Headers",
@@ -31,8 +29,7 @@ app.use(function (req, res, next) {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.static('public/images'));
-//below left here until certain routing will work without it
-// app.use(express.static(__dirname + "./../build"));
+app.use(express.static(__dirname + "./../build"));
 app.use(helmet());
 app.use(limiter);
 app.use(
