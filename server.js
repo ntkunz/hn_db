14e0cb8da08bc94f94d99f043cbb01385c98957d
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-08-10 07:16:58 -0700

    add req.header to res.header if req.header.origin is whitelisted

--- server.js
+++ server.js
@@ -25,11 +25,19 @@ const limiter = rateLimit({
 });
 
 app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", corsOptions.origin);
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept"
-	);
+	if (whitelist.indexOf(req.headers.origin) !== -1) {
+		res.header("Access-Control-Allow-Origin", req.headers.origin);
+		res.header(
+			"Access-Control-Allow-Headers",
+			"Origin, X-Requested-With, Content-Type, Accept"
+		);
+	}
+
+	// res.header("Access-Control-Allow-Origin", corsOptions.origin);
+	// res.header(
+	// 	"Access-Control-Allow-Headers",
+	// 	"Origin, X-Requested-With, Content-Type, Accept"
+	// );
 	next();
 });
 
