07dd185cdeeae9091c34daf9b9b0972ca1b49388
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-08-10 07:10:42 -0700

    update corsOptions.origin for res header server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const limiter = rateLimit({
 });
 
 app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", corsOptions);
+	res.header("Access-Control-Allow-Origin", corsOptions.origin);
 	res.header(
 		"Access-Control-Allow-Headers",
 		"Origin, X-Requested-With, Content-Type, Accept"
