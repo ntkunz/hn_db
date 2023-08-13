4caafefa78cf381dde95d84609cdcf72ae175534
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-08-13 09:07:30 -0700

    remove double require usresControllerV2 in authRoutesV2.js

--- routes/authRoutesV2.js
+++ routes/authRoutesV2.js
@@ -1,11 +1,7 @@
 const router = require("express").Router();
-const usersControllerV2 = require("../controllers/usersControllerV2");
+const usersControllerV2 = require("../controllers/usersControllerV2.js");
 
-const {
-	loginUser,
-	verifyLogin,
-} = require("../controllers/usersControllerV2.js");
-const { validate, loginSchema } = require("../modules/validation.js");
+// const { validate, loginSchema } = require("../modules/validation.js");
 
 // router.route("/").get(loginUser).post(validate(loginSchema), verifyLogin);
 router.route("/").get(usersControllerV2.loginUser).post(verifyLogin);
