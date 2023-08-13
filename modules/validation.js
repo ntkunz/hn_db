765b61f6f4a1c806b0a2ae3e67a5ebb6fbb4b02a
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-08-13 09:11:05 -0700

    comment out loginSchema validation.js as not callback function

--- modules/validation.js
+++ modules/validation.js
@@ -13,22 +13,23 @@ const validate = (schema) => async (req, res, next) => {
 	}
 };
 
-const loginSchema = yup.object({
-	body: yup.object({
-		email: yup.string().required("Email is required").email("Email is invalid"),
-		password: yup
-			.string()
-			.required("Password is required")
-			.min(8, "Password must be at least 6 characters")
-			.max(24, "Password must not exceed 24 characters")
-			.matches(/\d+/, "Password needs a number")
-			.matches(/[a-z]+/, "Password needs a lowercase letter")
-			.matches(/[A-Z]+/, "Password needs an uppercase letter")
-			.matches(/[!@#$%^&*()-+]+/, "Password needs special character"),
-	}),
-});
+// === I THINK THAT THE COMPILER DOESN'T LIKE THAT THIS ISN'T A CALLBACK FUNCTION, SO CANNOT EXPORT IT AS A MODULE ===
+// const loginSchema = yup.object({
+// 	body: yup.object({
+// 		email: yup.string().required("Email is required").email("Email is invalid"),
+// 		password: yup
+// 			.string()
+// 			.required("Password is required")
+// 			.min(8, "Password must be at least 6 characters")
+// 			.max(24, "Password must not exceed 24 characters")
+// 			.matches(/\d+/, "Password needs a number")
+// 			.matches(/[a-z]+/, "Password needs a lowercase letter")
+// 			.matches(/[A-Z]+/, "Password needs an uppercase letter")
+// 			.matches(/[!@#$%^&*()-+]+/, "Password needs special character"),
+// 	}),
+// });
 
 module.exports = {
 	validate,
-	loginSchema,
+	// loginSchema,
 };
