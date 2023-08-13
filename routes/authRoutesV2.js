543d3dacc76d34ccf0da1657714e895f5ab66a1f
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-08-13 09:14:13 -0700

    commented out get request authRoutesV2 as not setup yet

--- routes/authRoutesV2.js
+++ routes/authRoutesV2.js
@@ -4,4 +4,7 @@ const usersControllerV2 = require("../controllers/usersControllerV2.js");
 // const { validate, loginSchema } = require("../modules/validation.js");
 
 // router.route("/").get(loginUser).post(validate(loginSchema), verifyLogin);
-router.route("/").get(usersControllerV2.loginUser).post(verifyLogin);
+router
+	.route("/")
+	// .get(usersControllerV2.loginUser)
+	.post(verifyLogin);
