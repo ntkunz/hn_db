5abf20d4214945495a702a310681740c9f8b87ae
ntkunz <33107555+ntkunz@users.noreply.github.com>
2024-01-12 12:42:59 -0800

    update knexfile for production environment

--- knexfile.js
+++ knexfile.js
@@ -5,32 +5,32 @@ require("dotenv").config();
  */
 module.exports = {
 	//below used for local--------
-		client: "mysql2",
-		connection: {
-			host: "127.0.0.1",
-			database: process.env.DB_LOCAL_DBNAME,
-			user: process.env.DB_LOCAL_USER,
-			password: process.env.DB_LOCAL_PASSWORD,
-		},
-	};
+	// 	client: "mysql2",
+	// 	connection: {
+	// 		host: "127.0.0.1",
+	// 		database: process.env.DB_LOCAL_DBNAME,
+	// 		user: process.env.DB_LOCAL_USER,
+	// 		password: process.env.DB_LOCAL_PASSWORD,
+	// 	},
+	// };
 
 	// below used for deployment----------
-// 	client: "mysql2",
-// 	connection: {
-// 		host: process.env.DB_HOST,
-// 		user: process.env.DB_USER,
-// 		password: process.env.DB_PASSWORD,
-// 		database: process.env.DB_DBNAME,
-// 		port: 3306,
-// 	},
-// 	pool: {
-// 		min: 2,
-// 		max: 10,
-// 	},
-// 	migrations: {
-// 		directory: __dirname + "/migrations",
-// 	},
-// 	seeds: {
-// 		directory: __dirname + "/seeds",
-// 	},
-// };
+	client: "mysql2",
+	connection: {
+		host: process.env.DB_HOST,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_DBNAME,
+		port: 3306,
+	},
+	pool: {
+		min: 2,
+		max: 10,
+	},
+	migrations: {
+		directory: __dirname + "/migrations",
+	},
+	seeds: {
+		directory: __dirname + "/seeds",
+	},
+};
